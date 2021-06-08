2500c3e39ccea4564f7e9a8c775765f9742d6a27
Hvvvi <soungnl@gmail.com>
2021-06-08 15:57:57 +0900

    inital commit by sts

---
+++ KOSTA_3_final/src/main/java/com/kosta/KOSTA_3_final/Kosta3FinalApplication.java
@@ -0,0 +1,13 @@
+package com.kosta.KOSTA_3_final;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kosta3FinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kosta3FinalApplication.class, args);
+	}
+
+}
