3fac10498de647bac19f75e23f4940c477925b20
Hvvvi <soungnl@gmail.com>
2021-06-09 10:46:54 +0900

    일단 hr로 DB연결

--- KOSTA_3_final/src/main/java/com/kosta/KOSTA_3_final/Kosta3FinalApplication.java
+++ KOSTA_3_final/src/main/java/com/kosta/KOSTA_3_final/Kosta3FinalApplication.java
@@ -2,8 +2,10 @@ package com.kosta.KOSTA_3_final;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.kosta"})
 public class Kosta3FinalApplication {
 
 	public static void main(String[] args) {
