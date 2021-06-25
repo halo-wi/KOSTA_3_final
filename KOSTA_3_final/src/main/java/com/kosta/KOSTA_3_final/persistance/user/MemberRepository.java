95e1d941a7f61e2a0d1a6828fc9b774f6e822348
Sung-Hwi <75319719+halo-wi@users.noreply.github.com>
2021-06-25 10:08:13 +0900

    Update and rename UserPersistance.java to MemberRepository.java

--- KOSTA_3_final/src/main/java/com/kosta/KOSTA_3_final/persistance/user/MemberRepository.java
+++ KOSTA_3_final/src/main/java/com/kosta/KOSTA_3_final/persistance/user/MemberRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.repository.CrudRepository;
 import com.kosta.KOSTA_3_final.model.user.Member;
 
 
-public interface UserPersistance extends CrudRepository<Member, Long>{
+public interface MemberRepository extends CrudRepository<Member, Long>{
    public Optional<Member> findByEmail(String email);
    public Member findByCustomerName(String userEmail);
 	 public boolean existsByEmail(String email);
