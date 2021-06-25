6d50f73ad4252ee3bc9e852b5b66863444345ae4
Sung-Hwi <75319719+halo-wi@users.noreply.github.com>
2021-06-25 10:06:48 +0900

    Update Subscribe.java

--- KOSTA_3_final/src/main/java/com/kosta/KOSTA_3_final/model/subscribe/Subscribe.java
+++ KOSTA_3_final/src/main/java/com/kosta/KOSTA_3_final/model/subscribe/Subscribe.java
@@ -58,6 +58,10 @@ public class Subscribe {
    public PackageVO pack;
    
    
+   @JsonIgnore
+   @OneToMany(mappedBy = "subscribe", cascade = CascadeType.ALL)
+   List<Deilvery> delivery;
+   
    @CreationTimestamp
    @Column(name="payment_date")
    Date paymentDate;
