a28a480badaf7b176f7d00e0d943e659843b0044
Sung-Hwi <75319719+halo-wi@users.noreply.github.com>
2021-06-28 10:00:38 +0900

    리뷰 커밋 2

---
+++ KOSTA_3_final/src/main/java/com/kosta/KOSTA_3_final/service/subscribe/ReqPaymentScheduler.java
@@ -0,0 +1,38 @@
+package com.kosta.KOSTA_3_final.service.subscribe;
+
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.Trigger;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import org.springframework.scheduling.support.PeriodicTrigger;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ReqPaymentScheduler {
+    private ThreadPoolTaskScheduler scheduler;
+	@Autowired
+	SchedulePayment setSchedulePay;
+ 
+    public void stopScheduler() {
+        scheduler.shutdown();
+    }
+ 
+    public void startScheduler(int customer_uid, int price) {
+        scheduler = new ThreadPoolTaskScheduler();
+        scheduler.initialize();
+        // 스케쥴러가 시작되는 부분 
+        scheduler.schedule(getRunnable(customer_uid, price), getTrigger());
+    }
+ 
+    private Runnable getRunnable(int customer_uid, int price){
+        return () -> {
+        	setSchedulePay.schedulePay(customer_uid, price);
+        };
+    }
+ 
+    private Trigger getTrigger() {
+        // 작업 주기 설정 
+        return new PeriodicTrigger(1, TimeUnit.DAYS);
+    }
+}
