21d21229b156ac875355d077ec3daa7d2b0524cc
Hoosss <hsnam1211@naver.com>
2021-07-06 08:10:54 +0900

    남후승 3차 커밋

---
+++ final_project_subscribe/src/main/java/com/kosta/KOSTA_3_final/persistance/product_package/PackageRepository.java
@@ -0,0 +1,19 @@
+package com.kosta.KOSTA_3_final.persistance.product_package;
+
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import com.kosta.KOSTA_3_final.model.product_package.PackageVO;
+
+
+public interface PackageRepository extends CrudRepository<PackageVO, Long>{
+	@Query("select p from PackageVO p where rownum <=6 and packageType=?1")
+	public List<PackageVO> findByPackageType(int packageType);
+	
+	
+	@Query("select img from PackageVO")
+	public List<Integer> getImgName();
+}
