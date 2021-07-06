39528b9b85d248c07b16596fc50def1b647b53c0
Sung-Hwi <75319719+halo-wi@users.noreply.github.com>
2021-07-06 10:51:32 +0900

    Update ProductPackageController.java

--- KOSTA_3_final/src/main/java/com/kosta/KOSTA_3_final/controller/product_package/ProductPackageController.java
+++ KOSTA_3_final/src/main/java/com/kosta/KOSTA_3_final/controller/product_package/ProductPackageController.java
@@ -1,10 +1,6 @@
 package com.kosta.KOSTA_3_final.controller.product_package;
 
 
-import java.io.File;
-import java.io.IOException;
-
-import javax.swing.filechooser.FileSystemView;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -12,8 +8,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
 
 import com.kosta.KOSTA_3_final.model.product_package.ProductListVO;
 import com.kosta.KOSTA_3_final.service.product_package.ProductPackageService;
@@ -24,48 +18,57 @@ public class ProductPackageController {
 
 	@Autowired
 	ProductPackageService productService;
-	
+
 
 	@GetMapping("/product/customPackaging3")
 	public String selectProduct1(Model model) {
 		model.addAttribute("categorylist", productService.selectCategoryAll());
 		model.addAttribute("productlist", productService.selectProductAll());
-		
-		return "/product/customPackaging3";
+
+		return "/product/customPackaging4";
 	}
-	
+
 	@GetMapping("/product/adminMakeProductAndPackage")
 	public String adminMakeProductAndPackage(Model model) {
 		model.addAttribute("admin_check", "admin");
 		model.addAttribute("categorylist", productService.selectCategoryAll());
 		model.addAttribute("productlist", productService.selectProductAll());
 		model.addAttribute("companylist", productService.selectCompanyAll());
-		
+
 		return "/product/customPackaging4";
 	}
-	
+
 	@GetMapping("/product/productList")
 	public String selectProduct(Model model, String type, String keyword) {
 		model.addAttribute("productlist", productService.selectProductAll(type, keyword));
-		
+
 		return "/product/productList";
 	}
-	
-	@GetMapping("/product/customInsert")
+
+	@PostMapping("/product/customInsert")
 	public String insertPackage(Model model, @ModelAttribute ProductListVO productList) {
 		productService.insertPackage(productList);
-		
-		return "redirect:/product/customPackaging3";
-	}
-	
-	@PostMapping("/product/adminProductInsert")
-	public String adminInsertProduct(Model model, @ModelAttribute ProductListVO productList) throws Exception {
-		//productService.adminInsertProduct(productList);
-		System.out.println(productList);
-	    
+
 		return "redirect:/product/adminMakeProductAndPackage";
 	}
-	
-	
 
+	@PostMapping("/product/adminProductInsert")
+	public String adminProductInsert(Model model, @ModelAttribute ProductListVO productList) throws Exception {
+		productService.adminInsertProduct(productList);	    
+
+		return "redirect:/product/adminMakeProductAndPackage";
+	}
+
+	@GetMapping("/product/adminProductDelete")
+	public String adminDeleteProduct(Model model, @ModelAttribute ProductListVO productList) throws Exception {
+		productService.adminDeleteProduct(productList);	    
+
+		return "redirect:/product/adminMakeProductAndPackage";
+	}
+
+	@GetMapping("/subscribe")
+	public String aaa() {
+
+		return "/subscribe";
+	}
 }
